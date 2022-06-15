80672a16385f07d238aae84e126a32ceb5933966
Tyler Borton <tylercborton@gmail.com>
2022-06-15 10:45:13 -0400

    theaters router fix

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.getAll);.all(methodNotAllowed);
+router.route("/").get(controller.getAll).all(methodNotAllowed);
 
 
 module.exports = router;
