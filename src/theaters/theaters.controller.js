97d3f7219bcd26dd292a443cc3869723e833d292
Tyler Borton <tylercborton@gmail.com>
2022-06-15 11:03:09 -0400

    ready for testing

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -2,8 +2,8 @@ const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function getAll(req, res, next) {
-  const reviews = await service.getAll();
-  res.json({ data: reviews });
+  const theaters = await service.getAll();
+  res.json({ data: theaters });
 }
 
 module.exports={
