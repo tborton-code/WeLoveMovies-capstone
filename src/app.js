4e3d337b649ac687cf6c4bfa34776bb54323bd21
Tyler Borton <tylercborton@gmail.com>
2022-06-15 14:54:39 -0400

    added cors

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const moviesRouter = require("./movies/movies.router");
 const reviewsRouter = require("./reviews/reviews.router");
 const theatersRouter = require("./theaters/theaters.router");
 
 const notFound = require("./errors/notFound");
-const methodNotAllowed = require("./errors/methodNotAllowed");
 const errorHandler = require("./errors/errorHandler");
 
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
