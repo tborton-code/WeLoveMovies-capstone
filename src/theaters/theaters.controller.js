e27b22063da9c07d99f1eba5adb6326bf3b4b95b
Tyler Borton <tylercborton@gmail.com>
2022-06-15 10:47:28 -0400

    basic functions started

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,7 +1,11 @@
 const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+async function getAll(req, res, next) {
+  const reviews = await service.getAll();
+  res.json({ data: reviews });
+}
 
 module.exports={
-    
+    getAll,
 }
--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,7 +1,12 @@
 const knex = require("../db/connection");
 const mapProperties = require("../utils/map-properties");
 
+function getAll() {
+  return knex("theaters")
+    .select("*")
+    // .then(mapProperties);
+}
 
 module.exports = {
-
+    getAll,
 }
